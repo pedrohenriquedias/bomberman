ac87fdbdb6deb5373c88fd8428fa6caf133b4cb7
Pedro Henrique Dias <pedrohenriquedias@users.noreply.github.com>
2017-11-08 00:34:02 -0200

    Delete reverse-string.js from bomberman path

--- reverse-string.js
+++
@@ -1,8 +0,0 @@
-function reverseString(str) {
-  var splittedString = str.split("");
-  var reversedArray = splittedString.reverse();
-  var reversedString = reversedArray.join("");
-  return reversedString;
-}
-
-reverseString("hello");
