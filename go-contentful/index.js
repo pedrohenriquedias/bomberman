d7babfc974528fde5df10eeb85fd6bdff7385905
Alan Carpilovsky <alancarpilovsky@gmail.com>
2017-11-15 21:29:10 -0200

    Add go-contentful

---
+++ go-contentful/index.js
@@ -0,0 +1,7 @@
+function adaptTextToBookmarklet() {
+  document.getElementById('btn').textContent = 'Go Contentful'
+}
+
+function resetButtonText() {
+  document.getElementById('btn').textContent = 'this button'
+}
